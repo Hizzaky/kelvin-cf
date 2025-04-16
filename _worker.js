673f8d2e6c3f735be9f887d9cc0f0d865b99e3b8
Hizzaky <37980872+Hizzaky@users.noreply.github.com>
2025-04-16 21:35:30 +0900

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'e2c9fdb8-d748-4c7d-8361-b3e7f3d1fd82';
 
-const พร็อกซีไอพีs = ['103.101.153.32'];
+const พร็อกซีไอพีs = ['138.2.64.229'];
 // 178.128.91.137
 // 47.79.120.86
 // 128.199.213.153
